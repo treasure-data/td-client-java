83a802fd6987e816563a022ab6410cb14c128c22
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-03-15 21:33:49 +0900

    added unit test for HttpClientAdaptor#getServerStatus(..)

---
+++ src/test/java/com/treasure_data/client/TestGetServerStatus.java
@@ -0,0 +1,153 @@
+package com.treasure_data.client;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.json.simple.JSONValue;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.treasure_data.auth.TreasureDataCredentials;
+import com.treasure_data.client.HttpClientAdaptor.HttpConnectionImpl;
+import com.treasure_data.model.Request;
+import com.treasure_data.model.ServerStatusRequest;
+import com.treasure_data.model.ServerStatusResult;
+
+public class TestGetServerStatus {
+
+    @Before
+    public void setUp() throws Exception {
+        Properties props = System.getProperties();
+        props.load(this.getClass().getClassLoader().getResourceAsStream("treasure-data.properties"));
+    }
+
+    static class HttpConnectionImplforGetServerStatus01 extends HttpConnectionImpl {
+        @Override
+        void doGetRequest(Request<?> request, String path, Map<String, String> header,
+                Map<String, String> params) throws IOException {
+            // do nothing
+        }
+
+        @Override
+        int getResponseCode() throws IOException {
+            return HttpURLConnection.HTTP_OK;
+        }
+
+        @Override
+        String getResponseBody() throws IOException {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("status", "ok");
+            String jsonData = JSONValue.toJSONString(map);
+            return jsonData;
+        }
+
+        @Override
+        void disconnect() {
+            // do nothing
+        }
+    }
+
+    /**
+     * check normal behavior of client
+     */
+    @Test
+    public void testGetServerStatus01() throws Exception {
+        Config conf = new Config();
+        conf.setCredentials(new TreasureDataCredentials());
+        HttpClientAdaptor clientAdaptor = new HttpClientAdaptor(conf);
+        clientAdaptor.setConnection(new HttpConnectionImplforGetServerStatus01());
+        ServerStatusRequest request = new ServerStatusRequest();
+        ServerStatusResult result = clientAdaptor.getServerStatus(request);
+        assertEquals("ok", result.getMessage());
+    }
+
+    static class HttpConnectionImplforGetServerStatus02 extends HttpConnectionImpl {
+        @Override
+        void doGetRequest(Request<?> request, String path, Map<String, String> header,
+                Map<String, String> params) throws IOException {
+            // do nothing
+        }
+
+        @Override
+        int getResponseCode() throws IOException {
+            return HttpURLConnection.HTTP_OK;
+        }
+
+        @Override
+        String getResponseBody() throws IOException {
+            return "foobar"; // invalid JSON data
+        }
+
+        @Override
+        void disconnect() {
+            // do nothing
+        }
+    }
+
+    /**
+     * check behavior when receiving *invalid JSON data* as response body
+     */
+    @Test
+    public void testGetServerStatus02() throws Exception {
+        Config conf = new Config();
+        conf.setCredentials(new TreasureDataCredentials());
+        HttpClientAdaptor clientAdaptor = new HttpClientAdaptor(conf);
+        clientAdaptor.setConnection(new HttpConnectionImplforGetServerStatus02());
+        ServerStatusRequest request = new ServerStatusRequest();
+        try {
+            clientAdaptor.getServerStatus(request);
+            fail();
+        } catch (Throwable t) {
+            assertTrue(t instanceof ClientException);
+        }
+    }
+
+    static class HttpConnectionImplforGetServerStatus03 extends HttpConnectionImpl {
+        @Override
+        void doGetRequest(Request<?> request, String path, Map<String, String> header,
+                Map<String, String> params) throws IOException {
+            // do nothing
+        }
+
+        @Override
+        int getResponseCode() throws IOException {
+            return HttpURLConnection.HTTP_BAD_REQUEST;
+        }
+
+        @Override
+        String getResponseMessage() throws IOException {
+            return "";
+        }
+
+        @Override
+        String getResponseBody() throws IOException {
+            return "";
+        }
+
+        @Override
+        void disconnect() {
+            // do nothing
+        }
+    }
+
+    /**
+     * check behavior when receiving non-OK response code
+     */
+    @Test
+    public void testGetServerStatus03() throws Exception {
+        Config conf = new Config();
+        conf.setCredentials(new TreasureDataCredentials());
+        HttpClientAdaptor clientAdaptor = new HttpClientAdaptor(conf);
+        clientAdaptor.setConnection(new HttpConnectionImplforGetServerStatus03());
+        ServerStatusRequest request = new ServerStatusRequest();
+        ServerStatusResult result = clientAdaptor.getServerStatus(request);
+        assertTrue(result.getMessage() != "ok");
+    }
+}
