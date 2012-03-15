14a70f5e025d57a30c4d9e61b43f4c0747f1f199
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-03-15 20:33:23 +0900

    added test methods for HttpClientAdaptor#getServerStatus(..)

--- src/test/java/com/treasure_data/client/TestHttpClientAdaptor.java
+++ src/test/java/com/treasure_data/client/TestHttpClientAdaptor.java
@@ -1,8 +1,19 @@
 package com.treasure_data.client;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import org.json.simple.JSONValue;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -30,6 +41,7 @@ import com.treasure_data.model.ListJobsRequest;
 import com.treasure_data.model.ListJobsResult;
 import com.treasure_data.model.ListTablesRequest;
 import com.treasure_data.model.ListTablesResult;
+import com.treasure_data.model.Request;
 import com.treasure_data.model.ServerStatusRequest;
 import com.treasure_data.model.ServerStatusResult;
 import com.treasure_data.model.ShowJobRequest;
@@ -47,9 +59,34 @@ public class TestHttpClientAdaptor {
     }
 
     static class HttpConnectionImplforGetServerStatus01 extends HttpConnectionImpl {
-        
+        @Override
+        void doGetRequest(Request<?> request, String path, Map<String, String> header,
+                Map<String, String> params) throws IOException {
+            // do nothing
+        }
+
+        @Override
+        int getResponseCode() throws IOException {
+            return HttpURLConnection.HTTP_OK;
+        }
+
+        @Override
+        String getResponseBody() throws IOException {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("status", "ok");
+            String jsonData = JSONValue.toJSONString(map);
+            return jsonData;
+        }
+
+        @Override
+        void disconnect() {
+            // do nothing
+        }
     }
 
+    /**
+     * check normal behavior of client
+     */
     @Test
     public void testGetServerStatus01() throws Exception {
         Config conf = new Config();
@@ -58,7 +95,48 @@ public class TestHttpClientAdaptor {
         clientAdaptor.setConnection(new HttpConnectionImplforGetServerStatus01());
         ServerStatusRequest request = new ServerStatusRequest();
         ServerStatusResult result = clientAdaptor.getServerStatus(request);
-        System.out.println(result.getMessage());
+        assertEquals("ok", result.getMessage());
+    }
+
+    static class HttpConnectionImplforGetServerStatus02 extends HttpConnectionImpl {
+        @Override
+        void doGetRequest(Request<?> request, String path, Map<String, String> header,
+                Map<String, String> params) throws IOException {
+            // do nothing
+        }
+
+        @Override
+        int getResponseCode() throws IOException {
+            return HttpURLConnection.HTTP_OK;
+        }
+
+        @Override
+        String getResponseBody() throws IOException {
+            return "foobar"; // invalid JSON data
+        }
+
+        @Override
+        void disconnect() {
+            // do nothing
+        }
+    }
+
+    /**
+     * check behavior when receiving *invalid JSON data* as response body
+     */
+    @Test
+    public void testGetServerStatus02() throws Exception {
+        Config conf = new Config();
+        conf.setCredentials(new TreasureDataCredentials());
+        HttpClientAdaptor clientAdaptor = new HttpClientAdaptor(conf);
+        clientAdaptor.setConnection(new HttpConnectionImplforGetServerStatus02());
+        ServerStatusRequest request = new ServerStatusRequest();
+        try {
+            clientAdaptor.getServerStatus(request);
+            fail();
+        } catch (Throwable t) {
+            assertTrue(t instanceof ClientException);
+        }
     }
 
     @Test @Ignore
