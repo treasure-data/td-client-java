9c75faa80998cdb4ab276627c78f4e2318c6a3f5
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-03-17 17:09:06 +0900

    changed API of TreasureDataClient class

--- src/main/java/com/treasure_data/client/TreasureDataClient.java
+++ src/main/java/com/treasure_data/client/TreasureDataClient.java
@@ -17,6 +17,7 @@
 //
 package com.treasure_data.client;
 
+import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -25,6 +26,7 @@ import com.treasure_data.model.CreateDatabaseRequest;
 import com.treasure_data.model.CreateDatabaseResult;
 import com.treasure_data.model.CreateTableRequest;
 import com.treasure_data.model.CreateTableResult;
+import com.treasure_data.model.Database;
 import com.treasure_data.model.DeleteDatabaseRequest;
 import com.treasure_data.model.DeleteDatabaseResult;
 import com.treasure_data.model.DeleteTableRequest;
@@ -33,6 +35,8 @@ import com.treasure_data.model.ExportRequest;
 import com.treasure_data.model.ExportResult;
 import com.treasure_data.model.ImportRequest;
 import com.treasure_data.model.ImportResult;
+import com.treasure_data.model.Job;
+import com.treasure_data.model.JobResult;
 import com.treasure_data.model.KillJobRequest;
 import com.treasure_data.model.KillJobResult;
 import com.treasure_data.model.ListDatabasesRequest;
@@ -43,12 +47,14 @@ import com.treasure_data.model.ListTablesRequest;
 import com.treasure_data.model.ListTablesResult;
 import com.treasure_data.model.GetJobResultRequest;
 import com.treasure_data.model.GetJobResultResult;
+import com.treasure_data.model.ServerStatus;
 import com.treasure_data.model.ServerStatusRequest;
 import com.treasure_data.model.ServerStatusResult;
 import com.treasure_data.model.ShowJobRequest;
 import com.treasure_data.model.ShowJobResult;
 import com.treasure_data.model.SubmitJobRequest;
 import com.treasure_data.model.SubmitJobResult;
+import com.treasure_data.model.Table;
 
 public class TreasureDataClient {
     private static Logger LOG = Logger.getLogger(TreasureDataClient.class.getName());
@@ -80,19 +86,20 @@ public class TreasureDataClient {
 
     // Server Status API
 
-    public ServerStatusResult getServerStatus() throws ClientException {
-        return getServerStatus(new ServerStatusRequest());
+    public ServerStatus getServerStatus() throws ClientException {
+        return clientAdaptor.getServerStatus(
+                new ServerStatusRequest()).getServerStatus();
     }
 
     public ServerStatusResult getServerStatus(ServerStatusRequest request)
             throws ClientException {
-        return clientAdaptor.getServerStatus(request);
+        return clientAdaptor.getServerStatus(new ServerStatusRequest());
     }
 
     // Database API
 
-    public ListDatabasesResult listDatabases() throws ClientException {
-        return listDatabases(new ListDatabasesRequest());
+    public List<Database> listDatabases() throws ClientException {
+        return listDatabases(new ListDatabasesRequest()).getDatabases();
     }
 
     public ListDatabasesResult listDatabases(ListDatabasesRequest request)
@@ -100,9 +107,19 @@ public class TreasureDataClient {
         return clientAdaptor.listDatabases(request);
     }
 
+    public Database createDatabase(String databaseName)
+            throws ClientException {
+        return createDatabase(
+                new CreateDatabaseRequest(databaseName)).getDatabase();
+    }
+
     public CreateDatabaseResult createDatabase(CreateDatabaseRequest request)
             throws ClientException {
 	return clientAdaptor.createDatabase(request);
+    }
+
+    public void deleteDatabase(String databaseName) throws ClientException {
+        deleteDatabase(new DeleteDatabaseRequest(new Database(databaseName)));
     }
 
     public DeleteDatabaseResult deleteDatabase(DeleteDatabaseRequest request)
@@ -112,8 +129,24 @@ public class TreasureDataClient {
 
     // Table API
 
-    public ListTablesResult listTables(ListTablesRequest request) throws ClientException {
+    // TODO change type of return variable:
+    // List<Table> -> List<TableSummary>
+    public List<Table> listTables(String databaseName)
+            throws ClientException {
+        return listTables(new ListTablesRequest(
+                new Database(databaseName))).getTables();
+    }
+
+    public ListTablesResult listTables(ListTablesRequest request)
+            throws ClientException {
 	return clientAdaptor.listTables(request);
+    }
+
+    public Table createTable(String databaseName, String tableName)
+            throws ClientException {
+        CreateTableResult result = createTable(new CreateTableRequest(
+                new Database(databaseName), tableName, Table.Type.LOG));
+        return result.getTable();
     }
 
     public CreateTableResult createTable(CreateTableRequest request)
@@ -121,16 +154,37 @@ public class TreasureDataClient {
 	return clientAdaptor.createTable(request);
     }
 
+    // TODO #MN add it in next version
+    //public TableDescription describeTable(DescribeTableRequest request) throws ClientException;
+
+    // TODO #MN add it in next version
+    /**
+    TableDescription describeTable(String databaseName, String tableName) throws ClientException {
+        return describeTable(new DescribeTableRequest(databaseName, tableName));
+    }
+    */
+
+    public void deleteTable(String databaseName, String tableName)
+            throws ClientException {
+        deleteTable(new DeleteTableRequest(new Table(
+                new Database(databaseName), tableName, Table.Type.LOG)));
+    }
+
     public DeleteTableResult deleteTable(DeleteTableRequest request)
             throws ClientException {
 	return clientAdaptor.deleteTable(request);
     }
 
-    // Import and Export API
+    // TODO #MN add it in next version
+    //TableSchema updateTableSchema(UpdateTableSchemaRequest request) throws ClientException;
+
+    // Import API
 
     public ImportResult importData(ImportRequest request) throws ClientException {
         return clientAdaptor.importData(request);
     }
+
+    // Export API
 
     public ExportResult exportData(ExportRequest request) throws ClientException {
         return clientAdaptor.exportData(request);
@@ -138,23 +192,74 @@ public class TreasureDataClient {
 
     // Job API
 
-    public SubmitJobResult submitJob(SubmitJobRequest request) throws ClientException {
+    public void submitJob(Job job) throws ClientException {
+        submitJob(new SubmitJobRequest(job));
+    }
+
+    public SubmitJobResult submitJob(SubmitJobRequest request)
+            throws ClientException {
         return clientAdaptor.submitJob(request);
+    }
+
+    public List<Job> listJobs(long from, long to) throws ClientException {
+        return listJobs(new ListJobsRequest(from, to)).getJobs();
     }
 
     public ListJobsResult listJobs(ListJobsRequest request) throws ClientException {
         return clientAdaptor.listJobs(request);
     }
 
+    public void killJob(String jobID) throws ClientException {
+        killJob(new Job(jobID));
+    }
+
+    public void killJob(Job job) throws ClientException {
+        killJob(new KillJobRequest(job));
+    }
+
     public KillJobResult killJob(KillJobRequest request) throws ClientException {
         return clientAdaptor.killJob(request);
+    }
+
+    public void showJob(String jobID) throws ClientException {
+        showJob(new Job(jobID));
+    }
+
+    public void showJob(Job job) throws ClientException {
+        showJob(new ShowJobRequest(job));
     }
 
     public ShowJobResult showJob(ShowJobRequest request) throws ClientException {
         return clientAdaptor.showJob(request);
     }
 
+    // TODO #MN add it in next version
+    //JobDescription describeJob(DescribeJobRequest request) throws ClientException;
+
+    public JobResult getJobResult(Job job, JobResult.Format format)
+            throws ClientException {
+        return getJobResult(new GetJobResultRequest(
+                new JobResult(job, format))).getJobResult();
+    }
+
     public GetJobResultResult getJobResult(GetJobResultRequest request) throws ClientException {
         return clientAdaptor.getJobResult(request);
     }
+
+    // Job Scheduling API
+
+    // TODO #MN add it in next version
+    //ListScheduledJobResult listJobSchedules(ListScheduledJobRequest request) throws ClientException; // JobScheduleSummary < JobSchedule
+
+    // TODO #MN add it in next version
+    //List<JobSchedule> listJobSchedules() throws ClientException;
+
+    // TODO #MN add it in next version
+    //JobSchedule createJobSchedule(CreateJobScheduleRequest request) throws ClientException;
+
+    // TODO #MN add it in next version
+    //void deleteJobSchedule(DeleteJobScheduleRequest request) throws ClientException;
+
+    // TODO #MN add it in next version
+    //void deleteJobSchedule(String jobScheduleName) throws ClientException;
 }
