06677c816b44f244084af811e3806a71a795ac46
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-03-17 19:54:19 +0900

    changed test program for TreasureDataCredentials class

--- src/test/java/com/treasure_data/auth/TestTreasureDataCredentials.java
+++ src/test/java/com/treasure_data/auth/TestTreasureDataCredentials.java
@@ -1,7 +1,6 @@
 package com.treasure_data.auth;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -20,14 +19,4 @@ public class TestTreasureDataCredentials {
         assertTrue(credentials.getAPIKey() != null);
     }
 
-    @Test
-    public void testConstructor02() throws IOException {
-        try {
-            Properties props = new Properties();
-            new TreasureDataCredentials(props);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof NullPointerException);
-        }
-    }
 }
