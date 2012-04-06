b1455ec7a471c1c611993d9927063eb5cb3d62fd
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-04-06 09:23:59 +0900

    added test program for HttpClientAdaptor#getJobResult(..)

---
+++ src/test/java/com/treasure_data/client/TestGetJobResult.java
@@ -0,0 +1,34 @@
+package com.treasure_data.client;
+
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.treasure_data.auth.TreasureDataCredentials;
+import com.treasure_data.model.GetJobResultRequest;
+import com.treasure_data.model.GetJobResultResult;
+import com.treasure_data.model.Job;
+import com.treasure_data.model.JobResult;
+
+public class TestGetJobResult {
+    @Before
+    public void setUp() throws Exception {
+        Properties props = System.getProperties();
+        props.load(this.getClass().getClassLoader().getResourceAsStream("treasure-data.properties"));
+    }
+
+    @Test @Ignore
+    public void testGetJobResult00() throws Exception {
+        Config conf = new Config();
+        conf.setCredentials(new TreasureDataCredentials());
+        HttpClientAdaptor clientAdaptor = new HttpClientAdaptor(conf);
+
+        GetJobResultRequest request = new GetJobResultRequest(
+                new JobResult(new Job("26317")));
+        GetJobResultResult result = clientAdaptor.getJobResult(request);
+        System.out.println(result.getJob().getJobID());
+    }
+
+}
