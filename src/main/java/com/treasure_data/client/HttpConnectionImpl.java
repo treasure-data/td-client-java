084133096183cc66ac565f675f8cca7a6f471ba3
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-07-09 09:51:29 +0900

    added getInputStream method in HttpConnectionImpl class

--- src/main/java/com/treasure_data/client/HttpConnectionImpl.java
+++ src/main/java/com/treasure_data/client/HttpConnectionImpl.java
@@ -21,6 +21,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -203,6 +204,10 @@ public class HttpConnectionImpl {
         }
 
         return unpacker;
+    }
+
+    public InputStream getInputStream() throws IOException {
+        return conn.getInputStream();
     }
 
     private String getApiServerPath() {
