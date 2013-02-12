52b11e18a3caf6d81adc9cebc04f30b121e3cbe2
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-02-12 21:56:29 +0900

    refined TestListTables class

--- src/test/java/com/treasure_data/client/TestListTables.java
+++ src/test/java/com/treasure_data/client/TestListTables.java
@@ -1,11 +1,7 @@
 package com.treasure_data.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,18 +9,35 @@ import java.util.Map;
 import java.util.Properties;
 
 import org.json.simple.JSONValue;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.treasure_data.auth.TreasureDataCredentials;
-import com.treasure_data.client.HttpConnectionImpl;
 import com.treasure_data.model.Database;
 import com.treasure_data.model.ListTablesRequest;
 import com.treasure_data.model.ListTablesResult;
-import com.treasure_data.model.Request;
 import com.treasure_data.model.TableSummary;
 
-public class TestListTables {
+public class TestListTables extends GetMethodTestUtil {
+
+    private ListTablesRequest request;
+    private String databaseName;
+
+    @Before
+    public void createResources() throws Exception {
+        super.createResources();
+        databaseName = "testdb";
+        request = new ListTablesRequest(new Database(databaseName));
+    }
+
+    @After
+    public void deleteResources() throws Exception {
+        super.deleteResources();
+        databaseName = null;
+        request = null;
+    }
 
     @Test @Ignore
     public void testListTables00() throws Exception {
@@ -47,64 +60,13 @@ public class TestListTables {
         }
     }
 
-    static class HttpConnectionImplforListTables01 extends HttpConnectionImpl {
-        @Override
-        public void doGetRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @SuppressWarnings({ "rawtypes", "unchecked" })
-        @Override
-        public String getResponseBody() throws IOException {
-            Map map = new HashMap();
-            map.put("database", "testdb");
-            List tbls = new ArrayList();
-            Map m0 = new HashMap();
-            m0.put("type", "item");
-            m0.put("name", "foo");
-            m0.put("count", 13123233);
-            tbls.add(m0);
-            Map m1 = new HashMap();
-            m1.put("type", "item");
-            m1.put("name", "bar");
-            m1.put("count", 331232);
-            tbls.add(m1);
-            map.put("tables", tbls);
-            String jsonData = JSONValue.toJSONString(map);
-            return jsonData;
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
+    @Override
+    public void doBusinessLogic() throws Exception {
+        clientAdaptor.listTables(request);
     }
 
-    /**
-     * check normal behavior of client
-     */
-    @Test
-    public void testListTables01() throws Exception {
-        Properties props = System.getProperties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials());
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforListTables01());
-
-        String databaseName = "testdb";
-        ListTablesRequest request = new ListTablesRequest(new Database(databaseName));
+    @Override
+    public void checkNormalBehavior0() throws Exception {
         ListTablesResult result = clientAdaptor.listTables(request);
         List<TableSummary> tables = result.getTables();
         assertEquals(2, tables.size());
@@ -112,103 +74,22 @@ public class TestListTables {
         assertEquals("bar", tables.get(1).getName());
     }
 
-    static class HttpConnectionImplforListTables02 extends HttpConnectionImpl {
-        @Override
-        public void doGetRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "foobar"; // invalid JSON data
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check behavior when receiving *invalid JSON data* as response body
-     */
-    @Test
-    public void testListTables02() throws Exception {
-        Properties props = System.getProperties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials());
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforListTables02());
-
-        String databaseName = "testdb";
-        ListTablesRequest request = new ListTablesRequest(new Database(databaseName));
-        try {
-            clientAdaptor.listTables(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
-    }
-
-    static class HttpConnectionImplforListTables03 extends HttpConnectionImpl {
-        @Override
-        public void doGetRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_BAD_REQUEST;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "";
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check behavior when receiving non-OK response code
-     */
-    @Test
-    public void testListTables03() throws Exception {
-        Properties props = System.getProperties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials());
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforListTables03());
-
-        String databaseName = "testdb";
-        ListTablesRequest request = new ListTablesRequest(new Database(databaseName));
-        try {
-            clientAdaptor.listTables(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
+    @Override
+    public String getJSONTextForChecking() {
+        Map map = new HashMap();
+        map.put("database", "testdb");
+        List tbls = new ArrayList();
+        Map m0 = new HashMap();
+        m0.put("type", "item");
+        m0.put("name", "foo");
+        m0.put("count", 13123233);
+        tbls.add(m0);
+        Map m1 = new HashMap();
+        m1.put("type", "item");
+        m1.put("name", "bar");
+        m1.put("count", 331232);
+        tbls.add(m1);
+        map.put("tables", tbls);
+        return JSONValue.toJSONString(map);
     }
 }
