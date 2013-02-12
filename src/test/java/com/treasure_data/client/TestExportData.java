690d259e4174efb087a1ab50cc5ecbe8e90108cd
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-02-12 21:41:43 +0900

    refined TestExportData class

--- src/test/java/com/treasure_data/client/TestExportData.java
+++ src/test/java/com/treasure_data/client/TestExportData.java
@@ -1,29 +1,46 @@
 package com.treasure_data.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 import org.json.simple.JSONValue;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.treasure_data.auth.TreasureDataCredentials;
-import com.treasure_data.client.HttpConnectionImpl;
 import com.treasure_data.model.Database;
 import com.treasure_data.model.ExportRequest;
 import com.treasure_data.model.ExportResult;
 import com.treasure_data.model.Job;
-import com.treasure_data.model.Request;
 import com.treasure_data.model.Table;
 
-public class TestExportData {
+public class TestExportData extends PostMethodTestUtil {
+
+    private String databaseName;
+    private String tableName;
+    private ExportRequest request;
+
+    @Before
+    public void createResources() throws Exception {
+        super.createResources();
+        databaseName = "testdb";
+        tableName = "testtbl";
+        request = new ExportRequest(new Table(new Database(databaseName),
+                tableName), "s3", "bucket1", "json.gz", "xxx", "yyy");
+    }
+
+    @After
+    public void deleteResources() throws Exception {
+        super.deleteResources();
+        databaseName = null;
+        tableName = null;
+        request = null;
+    }
 
     @Test @Ignore
     public void testExportData00() throws Exception {
@@ -45,160 +62,24 @@ public class TestExportData {
         System.out.println(job.getJobID());
     }
 
-    static class HttpConnectionImplforExportData01 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            Map<String, String> map = new HashMap<String, String>();
-            map.put("job_id", "12345");
-            map.put("database", "mugadb");
-            String jsonData = JSONValue.toJSONString(map);
-            return jsonData;
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check normal behavior of client
-     */
-    @Test
-    public void testExportData01() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforExportData01());
-
-        Database database = new Database("mugadb");
-        Table table = new Table(database, "mugatbl");
-        ExportRequest request = new ExportRequest(
-                table, "s3", "bucket1", "json.gz", "xxx", "yyy");
+    @Override
+    public void checkNormalBehavior0() throws Exception {
         ExportResult result = clientAdaptor.exportData(request);
         Job job = result.getJob();
-        assertEquals(database.getName(), job.getDatabase().getName());
+        assertEquals(databaseName, job.getDatabase().getName());
+        assertEquals("12345", job.getJobID());
     }
 
-    static class HttpConnectionImplforExportData02 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "foobar"; // invalid JSON data
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
+    @Override
+    public String getJSONTextForChecking() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("job_id", "12345");
+        map.put("database", databaseName);
+        return JSONValue.toJSONString(map);
     }
 
-    /**
-     * check behavior when receiving *invalid JSON data* as response body
-     */
-    @Test
-    public void testExportData02() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforExportData02());
-
-        try {
-            Database database = new Database("mugadb");
-            Table table = new Table(database, "mugatbl");
-            ExportRequest request = new ExportRequest(
-                    table, "s3", "bucket1", "json.gz", "xxx", "yyy");
-            clientAdaptor.exportData(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
-    }
-
-    static class HttpConnectionImplforExportData03 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_BAD_REQUEST;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "";
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check behavior when receiving non-OK response code
-     */
-    @Test
-    public void testExportData03() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforExportData03());
-
-        try {
-            Database database = new Database("mugadb");
-            Table table = new Table(database, "mugatbl");
-            ExportRequest request = new ExportRequest(
-                    table, "ex", "bucket1", "json.gz", "xxx", "yyy");
-            clientAdaptor.exportData(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
+    @Override
+    public void doBusinessLogic() throws Exception {
+        clientAdaptor.exportData(request);
     }
 }
