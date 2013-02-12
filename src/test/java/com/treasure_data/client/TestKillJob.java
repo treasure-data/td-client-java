d9c2e28f5d982ebe4de07be5ec95dad301203b0c
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-02-12 20:47:33 +0900

    refined TestKillJob class

--- src/test/java/com/treasure_data/client/TestKillJob.java
+++ src/test/java/com/treasure_data/client/TestKillJob.java
@@ -1,28 +1,41 @@
 package com.treasure_data.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 import org.json.simple.JSONValue;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.treasure_data.auth.TreasureDataCredentials;
-import com.treasure_data.client.HttpConnectionImpl;
 import com.treasure_data.model.Job;
 import com.treasure_data.model.JobSummary;
 import com.treasure_data.model.KillJobRequest;
 import com.treasure_data.model.KillJobResult;
-import com.treasure_data.model.Request;
 
-public class TestKillJob {
+public class TestKillJob extends PostMethodTestUtil {
+
+    private Job job;
+    private KillJobRequest request;
+
+    @Before
+    public void createResources() throws Exception {
+        super.createResources();
+        job = new Job("47555");
+        request = new KillJobRequest(job);
+    }
+
+    @After
+    public void deleteResources() throws Exception {
+        super.deleteResources();
+        job = null;
+        request = null;
+    }
 
     @Test @Ignore
     public void testKillJob00() throws Exception {
@@ -37,154 +50,23 @@ public class TestKillJob {
         System.out.println(result.getJobID());
     }
 
-    static class HttpConnectionImplforKillJob01 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            Map<String, String> map = new HashMap<String, String>();
-            map.put("former_status", "running");
-            map.put("job_id", "12345");
-            String jsonData = JSONValue.toJSONString(map);
-            return jsonData;
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check normal behavior of client
-     */
-    @Test
-    public void testKillJob01() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforKillJob01());
-
-        String jobID = "12345";
-        KillJobRequest request = new KillJobRequest(new Job(jobID));
+    @Override
+    public void checkNormalBehavior0() throws Exception {
         KillJobResult result = clientAdaptor.killJob(request);
-        assertEquals(jobID, result.getJobID());
+        assertEquals("12345", result.getJobID());
         assertEquals(JobSummary.Status.RUNNING, result.getFormerStatus());
     }
 
-    static class HttpConnectionImplforKillJob02 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "foobar"; // invalid JSON data
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
+    @Override
+    public String getJSONTextForChecking() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("former_status", "running");
+        map.put("job_id", "12345");
+        return JSONValue.toJSONString(map);
     }
 
-    /**
-     * check behavior when receiving *invalid JSON data* as response body
-     */
-    @Test
-    public void testKillJob02() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforKillJob02());
-
-        try {
-            String jobID = "12345";
-            KillJobRequest request = new KillJobRequest(new Job(jobID));
-            clientAdaptor.killJob(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
-    }
-
-    static class HttpConnectionImplforKillJob03 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_BAD_REQUEST;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "";
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check behavior when receiving non-OK response code
-     */
-    @Test
-    public void testKillJob03() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforKillJob03());
-
-        try {
-            String jobID = "12345";
-            KillJobRequest request = new KillJobRequest(new Job(jobID));
-            clientAdaptor.killJob(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
+    @Override
+    public void doBusinessLogic() throws Exception {
+        clientAdaptor.killJob(request);
     }
 }
