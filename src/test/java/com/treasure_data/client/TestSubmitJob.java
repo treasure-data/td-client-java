5602c245e4506dc01e25480ffc76aa93fb9d3f48
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-02-12 20:56:42 +0900

    refined TestSubmitJob class

--- src/test/java/com/treasure_data/client/TestSubmitJob.java
+++ src/test/java/com/treasure_data/client/TestSubmitJob.java
@@ -1,28 +1,41 @@
 package com.treasure_data.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 import org.json.simple.JSONValue;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.treasure_data.auth.TreasureDataCredentials;
-import com.treasure_data.client.HttpConnectionImpl;
 import com.treasure_data.model.Database;
 import com.treasure_data.model.Job;
-import com.treasure_data.model.Request;
 import com.treasure_data.model.SubmitJobRequest;
 import com.treasure_data.model.SubmitJobResult;
 
-public class TestSubmitJob {
+public class TestSubmitJob extends PostMethodTestUtil {
+
+    private Job job;
+    private SubmitJobRequest request;
+
+    @Before
+    public void createResources() throws Exception {
+        super.createResources();
+        job = new Job(new Database("testdb"), "select * from testtbl", null);
+        request = new SubmitJobRequest(job);
+    }
+
+    @After
+    public void deleteResources() throws Exception {
+        super.deleteResources();
+        job = null;
+        request = null;
+    }
 
     @Test @Ignore
     public void testSubmitJob00() throws Exception {
@@ -40,157 +53,23 @@ public class TestSubmitJob {
         System.out.println(job.getJobID());
     }
 
-    static class HttpConnectionImplforSubmitJob01 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            Map<String, String> map = new HashMap<String, String>();
-            map.put("job_id", "12345");
-            map.put("database", "mugadb");
-            String jsonData = JSONValue.toJSONString(map);
-            return jsonData;
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check normal behavior of client
-     */
-    @Test
-    public void testSubmitJob01() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforSubmitJob01());
-
-        Database database = new Database("mugadb");
-        String q = "select * from mugatbl";
-        SubmitJobRequest request = new SubmitJobRequest(new Job(database, q, null));
+    @Override
+    public void checkNormalBehavior0() throws Exception {
         SubmitJobResult result = clientAdaptor.submitJob(request);
         Job job = result.getJob();
-        assertEquals(database.getName(), job.getDatabase().getName());
+        assertEquals("12345", job.getJobID());
     }
 
-    static class HttpConnectionImplforSubmitJob02 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "foobar"; // invalid JSON data
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
+    @Override
+    public String getJSONTextForChecking() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("job_id", "12345");
+        map.put("database", "mugadb");
+        return JSONValue.toJSONString(map);
     }
 
-    /**
-     * check behavior when receiving *invalid JSON data* as response body
-     */
-    @Test
-    public void testSubmitJob02() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforSubmitJob02());
-
-        try {
-            Database database = new Database("mugadb");
-            String q = "select * from mugatbl";
-            SubmitJobRequest request = new SubmitJobRequest(new Job(database, q, null));
-            clientAdaptor.submitJob(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
-    }
-
-    static class HttpConnectionImplforSubmitJob03 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_BAD_REQUEST;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "";
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check behavior when receiving non-OK response code
-     */
-    @Test
-    public void testSubmitJob03() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforSubmitJob03());
-
-        try {
-            Database database = new Database("mugadb");
-            String q = "select * from mugatbl";
-            SubmitJobRequest request = new SubmitJobRequest(new Job(database, q, null));
-            clientAdaptor.submitJob(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
+    @Override
+    public void doBusinessLogic() throws Exception {
+        clientAdaptor.submitJob(request);
     }
 }
