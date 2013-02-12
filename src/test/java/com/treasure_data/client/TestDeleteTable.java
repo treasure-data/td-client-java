e91d93ba64849004f75a0ff9d8f1f2a9cfba46ba
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-02-12 20:27:32 +0900

    refined TestDeleteTable class

--- src/test/java/com/treasure_data/client/TestDeleteTable.java
+++ src/test/java/com/treasure_data/client/TestDeleteTable.java
@@ -1,21 +1,18 @@
 package com.treasure_data.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 import org.json.simple.JSONValue;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.treasure_data.auth.TreasureDataCredentials;
-import com.treasure_data.client.HttpConnectionImpl;
 import com.treasure_data.model.CreateDatabaseRequest;
 import com.treasure_data.model.CreateDatabaseResult;
 import com.treasure_data.model.CreateTableRequest;
@@ -24,10 +21,29 @@ import com.treasure_data.model.Database;
 import com.treasure_data.model.DeleteDatabaseRequest;
 import com.treasure_data.model.DeleteTableRequest;
 import com.treasure_data.model.DeleteTableResult;
-import com.treasure_data.model.Request;
 import com.treasure_data.model.Table;
 
-public class TestDeleteTable {
+public class TestDeleteTable extends PostMethodTestUtil {
+
+    private String databaseName;
+    private String tableName;
+    private DeleteTableRequest request;
+
+    @Before
+    public void createResources() throws Exception {
+        super.createResources();
+        databaseName = "testdb";
+        tableName = "testtbl";
+        request = new DeleteTableRequest(new Table(
+                new Database(databaseName), tableName, Table.Type.LOG));
+    }
+
+    @After
+    public void deleteResources() throws Exception {
+        super.deleteResources();
+        databaseName = null;
+        request = null;
+    }
 
     @Test @Ignore
     public void testDeleteTable00() throws Exception {
@@ -56,161 +72,24 @@ public class TestDeleteTable {
         }
     }
 
-    static class HttpConnectionImplforDeleteTable01 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            Map<String, String> map = new HashMap<String, String>();
-            map.put("database", "testdb");
-            map.put("table", "testtbl");
-            map.put("type", "log");
-            String jsonData = JSONValue.toJSONString(map);
-            return jsonData;
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check normal behavior of client
-     */
-    @Test
-    public void testDeleteTable01() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforDeleteTable01());
-
-        String databaseName = "testdb";
-        String tableName = "testtbl";
-        DeleteTableRequest request = new DeleteTableRequest(new Table(
-                new Database(databaseName), tableName, Table.Type.LOG));
+    @Override
+    public void checkNormalBehavior0() throws Exception {
         DeleteTableResult result = clientAdaptor.deleteTable(request);
         assertEquals(databaseName, result.getDatabase().getName());
         assertEquals(tableName, result.getTableName());
     }
 
-    static class HttpConnectionImplforDeleteTable02 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_OK;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "foobar"; // invalid JSON data
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
+    @Override
+    public String getJSONTextForChecking() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("database", "testdb");
+        map.put("table", "testtbl");
+        map.put("type", "log");
+        return JSONValue.toJSONString(map);
     }
 
-    /**
-     * check behavior when receiving *invalid JSON data* as response body
-     */
-    @Test
-    public void testDeleteTable02() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforDeleteTable02());
-
-        String databaseName = "testdb";
-        String tableName = "testtble";
-        DeleteTableRequest request = new DeleteTableRequest(new Table(
-                new Database(databaseName), tableName, Table.Type.LOG));
-        try {
-            clientAdaptor.deleteTable(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
-    }
-
-    static class HttpConnectionImplforDeleteTable03 extends HttpConnectionImpl {
-        @Override
-        public void doPostRequest(Request<?> request, String path, Map<String, String> header,
-                Map<String, String> params) throws IOException {
-            // do nothing
-        }
-
-        @Override
-        public int getResponseCode() throws IOException {
-            return HttpURLConnection.HTTP_BAD_REQUEST;
-        }
-
-        @Override
-        public String getResponseMessage() throws IOException {
-            return "";
-        }
-
-        @Override
-        public String getResponseBody() throws IOException {
-            return "";
-        }
-
-        @Override
-        public void disconnect() {
-            // do nothing
-        }
-    }
-
-    /**
-     * check behavior when receiving non-OK response code
-     */
-    @Test
-    public void testDeleteTable03() throws Exception {
-        Properties props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("mock-treasure-data.properties"));
-        Config conf = new Config();
-        conf.setCredentials(new TreasureDataCredentials(props));
-        DefaultClientAdaptorImpl clientAdaptor = new DefaultClientAdaptorImpl(conf);
-        clientAdaptor.setConnection(new HttpConnectionImplforDeleteTable03());
-
-        String databaseName = "testdb";
-        String tableName = "testtble";
-        DeleteTableRequest request = new DeleteTableRequest(new Table(
-                new Database(databaseName), tableName, Table.Type.LOG));
-        try {
-            clientAdaptor.deleteTable(request);
-            fail();
-        } catch (Throwable t) {
-            assertTrue(t instanceof ClientException);
-        }
+    @Override
+    public void doBusinessLogic() throws Exception {
+        clientAdaptor.deleteTable(request);
     }
 }
