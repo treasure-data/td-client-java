2f0b811149b68b526559962d479f1e01cafbfb7e
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-10-25 02:51:04 +0900

    added new table model classes: {Item,Log}Table

---
+++ src/main/java/com/treasure_data/model/ItemTable.java
@@ -0,0 +1,38 @@
+//
+// Java Client Library for Treasure Data Cloud
+//
+// Copyright (C) 2011 - 2013 Muga Nishizawa
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+package com.treasure_data.model;
+
+public class ItemTable extends Table {
+
+    protected String primaryKey;
+    protected DataType primaryKeyType;
+
+    public ItemTable(Database database, String name, String primaryKey, DataType primaryKeyType) {
+        super(database, name, Table.Type.ITEM);
+        this.primaryKey = primaryKey;
+        this.primaryKeyType = primaryKeyType;
+    }
+
+    public String getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public DataType getPrimaryKeyType() {
+        return primaryKeyType;
+    }
+}
