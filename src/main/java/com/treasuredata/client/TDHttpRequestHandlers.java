0fd00f96926a4049cc292798d9edbbfea02bedf8
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2023-01-26 10:00:20 +0900

    Make TDHttpRequestHandler FunctionalInterface (#219)

--- src/main/java/com/treasuredata/client/TDHttpRequestHandlers.java
+++ src/main/java/com/treasuredata/client/TDHttpRequestHandlers.java
@@ -1,7 +1,6 @@
 package com.treasuredata.client;
 
 import com.google.common.base.Function;
-import okhttp3.Response;
 import okhttp3.ResponseBody;
 
 import java.io.InputStream;
@@ -15,37 +14,15 @@ public class TDHttpRequestHandlers
     {
     }
 
-    public static final TDHttpRequestHandler<String> stringContentHandler = new TDHttpRequestHandler<String>()
-    {
-        @Override
-        public String onSuccess(Response response)
-                throws Exception
-        {
-            return response.body().string();
-        }
-    };
+    public static final TDHttpRequestHandler<String> stringContentHandler = response -> response.body().string();
 
-    public static final TDHttpRequestHandler<byte[]> byteArrayContentHandler = new TDHttpRequestHandler<byte[]>()
-    {
-        @Override
-        public byte[] onSuccess(Response response)
-                throws Exception
-        {
-            return response.body().bytes();
-        }
-    };
+    public static final TDHttpRequestHandler<byte[]> byteArrayContentHandler = response -> response.body().bytes();
 
     public static final <Result> TDHttpRequestHandler<Result> newByteStreamHandler(final Function<InputStream, Result> handler)
     {
-        return new TDHttpRequestHandler<Result>()
-        {
-            @Override
-            public Result onSuccess(Response response)
-                    throws Exception
-            {
-                try (ResponseBody body = response.body()) {
-                    return handler.apply(body.byteStream());
-                }
+        return response -> {
+            try (ResponseBody body = response.body()) {
+                return handler.apply(body.byteStream());
             }
         };
     }
